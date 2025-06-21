ce43e39895c14a97b521d2a5b3128331d005e43e
GitHub Action <action@github.com>
2025-06-21 10:41:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/jalpatel11/jetbrainscopilot/MyBundle.kt
+++ src/main/kotlin/com/github/jalpatel11/jetbrainscopilot/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jalpatel11.jetbrainscopilot
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jalpatel11/jetbrainscopilot/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/jalpatel11/jetbrainscopilot/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.jalpatel11.jetbrainscopilot.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
